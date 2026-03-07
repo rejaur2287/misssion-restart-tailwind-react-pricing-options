2470597f3bed287ddfb575c0a2a1562a5066eafc
rejaur2287 <rezaurewubd@gmail.com>
2026-03-08 00:41:28 +0600

    Project initialization with tailwind,daisyUi, Navabar cration

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import DaisyNav from "./components/DaisyNav/DaisyNav";
+
+import NavBar from "./components/Navbar/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+      {/* <DaisyNav></DaisyNav> */}
+    </>
+  );
+}
+
+export default App;
