676bcbd434e07eee14a58a8cefa581065c3ca857
rejaur2287 <rezaurewubd@gmail.com>
2026-03-11 00:39:39 +0600

    Recharts and axios implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import DaisyNav from "./components/DaisyNav/DaisyNav";
 
 import NavBar from "./components/Navbar/NavBar";
 import PricingOptions from "./components/PricingOptions/PricingOptions";
+import ResultChart from "./components/ResultChart/ResultChart";
+import axios from "axios";
+import MarksChart from "./components/MarksChart/MarksChart";
 
 const pricingPromise = fetch("pricingData.json").then((res) => res.json());
+const marksPromise = axios.get("marksData.json");
 
 function App() {
   return (
@@ -22,6 +26,16 @@ function App() {
         >
           <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
         </Suspense>
+
+        <Suspense
+          fallback={
+            <span className="loading loading-spinner loading-lg"></span>
+          }
+        >
+          <MarksChart marksPromise={marksPromise}></MarksChart>
+        </Suspense>
+
+        <ResultChart></ResultChart>
       </main>
     </>
   );
